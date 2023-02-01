eeaadc0e470758e5b6857c21e78f69771db27077
Ruslan <r8446101@gmail.com>
2023-02-01 21:17:15 +0300

    Create model Category and Product

--- sto/zakaz/models.py
+++ sto/zakaz/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-class ZakazVidaUslRab(models.Model):
+class DataTimeMixin:
+    data_created = models.DateTimeField(auto_now_add=True)
+    data_updated = models.DateTimeField(auto_now=True)
+class ZakazVidaUslRab(models.Model, DataTimeMixin):
     name = models.CharField("Имя", max_length=150)
     telefon = models.CharField("Телефон", max_length=150)
     data = models.DateField("Дата")
